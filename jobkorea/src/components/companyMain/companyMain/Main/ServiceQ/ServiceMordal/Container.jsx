b46ead6635a826606e1333e9304f3b45dc3773af
nyes <pys9839@gmail.com>
2022-12-11 16:44:17 +0900

    1211 Company Mainpage UI
    ServiceMordal Structure Edit,
    Prev, Next button Add

--- jobkorea/src/components/companyMain/companyMain/Main/ServiceQ/ServiceMordal/Container.jsx
+++ jobkorea/src/components/companyMain/companyMain/Main/ServiceQ/ServiceMordal/Container.jsx
@@ -1,26 +1,21 @@
-import ServiceModalComponent1 from "./Component 1";
-import ServiceModalComponent2 from "./Component 2";
-import ServiceModalComponent3 from "./Component 3";
-import ServiceModalComponent4 from "./Component 4";
-import ServiceModalComponent5 from "./Component 5";
-import ServiceModalComponent6 from "./Component 6";
-import ServiceModalComponent7 from "./Component 7";
-import ServiceModalComponent8 from "./Component 8";
+import ServiceModalComponent from "./Component";
 
-const imsiData = [
+const data = [
   {
     title: "1. 채용공고를 내고 싶은데 절차가 어떻게 되나요?",
     text: [
       "잡코리아에 채용공고 등록을 원하시면 기업회원 ID로 로그인을 먼저 해주세요.",
       {
-        list: [
+        divList: [
           "기업 회원가입",
           "로그인",
           "채용공고 등록양식 작성",
           "등록 완료",
         ],
       },
-      "ID가 없으시면 먼저 회원가입부터 진행하여 주세요. 회원가입은 무료입니다.\n로그인하신 후 채용공고등록 페이지로 이동하셔서 채용공고 내용을 입력하신 후 채용공고 등록완료 버튼을 클릭하시면 됩니다.",
+      "ID가 없으시면 먼저 회원가입부터 진행하여 주세요. 회원가입은 무료입니다.",
+      "로그인하신 후 채용공고등록 페이지로 이동하셔서 채용공고 내용을 입력하신 후 채용공고 등록완료 버튼을 클릭하시면 됩니다.",
+      "-",
       "채용공고등록에 대한 별도 등록비용은 없으며, 무료로 등록하신 채용공고는 선택하신 업·직종 및 상세지역 등의 채용정보 리스트에서 게재되어 구직자에게 노출됩니다. 보다 효과적으로 구직자에게 채용공고 노출을 원하시면 차별화 된 노출위치와 노출효과를 제공하는 잡코리아 유료 채용광고 서비스를 함께 이용하시면 좋습니다.",
     ],
   },
@@ -57,17 +52,26 @@ const imsiData = [
           "맞춤인재 - Smart Match를 통해 내가 등록한 채용공고에 가장 적합한 이력서 확인 가능",
         ],
       },
-      "입사지원자의 경우, 예비합격 여부와 점수는 지원자 및 개인회원에게 공개되지 않습니다. \n즉 지원자 관리용으로 자유롭게 이용하실 수 있습니다.",
+      "입사지원자의 경우, 예비합격 여부와 점수는 지원자 및 개인회원에게 공개되지 않습니다.",
+      "즉 지원자 관리용으로 자유롭게 이용하실 수 있습니다.",
       "채용관리의 지원자 보기에서는 사전 인터뷰 답변 내용, 온라인 인·적성 검사 결과, 온라인 추천서 등도 함께 확인할 수 있습니다.",
     ],
   },
   {
-    title: "입사제의 서비스는 무엇인가요?",
-    text: "기업에서 이력서 검색을 하신 후에 관심있는 개인회원에게 입사제의를 할 수 있습니다. \n입사제의를 하게 되면, 해당 개인회원에게 자동으로 메일이 발송되며 개인회원은 기업에서 발송한 메일을 보고 응답할 수 있습니다.\n 회사에 적합한 인재가 있다면, 입사제의 서비스를 이용하시고 좋은 인재를 만나시길 바랍니다.",
+    title: "4. 입사제의 서비스는 무엇인가요?",
+    text: [
+      "기업에서 이력서 검색을 하신 후에 관심있는 개인회원에게 입사제의를 할 수 있습니다. ",
+      "입사제의를 하게 되면, 해당 개인회원에게 자동으로 메일이 발송되며 개인회원은 기업에서 발송한 메일을 보고 응답할 수 있습니다.",
+      "회사에 적합한 인재가 있다면, 입사제의 서비스를 이용하시고 좋은 인재를 만나시길 바랍니다.",
+    ],
   },
   {
-    title: "관심있는 이력서를 스크랩할 수 있나요?",
-    text: "이력서 검색을 하시다가 관심있는 이력서가 있을 경우에는 이력서 보기 화면 우측 메뉴를 보시면 [☆ 인재 스크랩] 버튼이 있습니다.\n [☆ 인재 스크랩] 버튼을 한번 클릭하면 스크랩하실 수 있습니다.\n 일반기업, 헤드헌팅, 파견대행 회원으로 각각 로그인 하신 후 스크랩 할 수 있습니다.",
+    title: "5. 관심있는 이력서를 스크랩할 수 있나요?",
+    text: [
+      "이력서 검색을 하시다가 관심있는 이력서가 있을 경우에는 이력서 보기 화면 우측 메뉴를 보시면 [☆ 인재 스크랩] 버튼이 있습니다.",
+      "[☆ 인재 스크랩] 버튼을 한번 클릭하면 스크랩하실 수 있습니다.",
+      "일반기업, 헤드헌팅, 파견대행 회원으로 각각 로그인 하신 후 스크랩 할 수 있습니다.",
+    ],
   },
   {
     title: "6. 스크랩한 인재는 어디에서 확인이 가능한가요?",
@@ -103,48 +107,67 @@ const imsiData = [
   },
 ];
 
-const ServiceModalContainer = ({ setMordalC, mdIndex }) => {
+const ServiceModalContainer = ({ setMordalC, mdIndex, setMdIndex }) => {
   const onClick = () => {
     setMordalC(false);
   };
-  // return <ServiceModalComponent onClick={onClick}></ServiceModalComponent>;
-  switch (mdIndex) {
-    case 1:
-      return (
-        <ServiceModalComponent1 onClick={onClick}></ServiceModalComponent1>
-      );
-    case 2:
-      return (
-        <ServiceModalComponent2
-          onClick={onClick}
-          data={imsiData[1]}
-        ></ServiceModalComponent2>
-      );
-    case 3:
-      return (
-        <ServiceModalComponent3 onClick={onClick}></ServiceModalComponent3>
-      );
-    case 4:
-      return (
-        <ServiceModalComponent4 onClick={onClick}></ServiceModalComponent4>
-      );
-    case 5:
-      return (
-        <ServiceModalComponent5 onClick={onClick}></ServiceModalComponent5>
-      );
-    case 6:
-      return (
-        <ServiceModalComponent6 onClick={onClick}></ServiceModalComponent6>
-      );
-    case 7:
-      return (
-        <ServiceModalComponent7 onClick={onClick}></ServiceModalComponent7>
-      );
-    case 8:
-      return (
-        <ServiceModalComponent8 onClick={onClick}></ServiceModalComponent8>
-      );
-  }
+  const prevClick = () => {
+    if (mdIndex == 1) return;
+    setMdIndex(mdIndex - 1);
+  };
+  const nextClick = () => {
+    if (mdIndex == 8) return;
+    setMdIndex(mdIndex + 1);
+  };
+  return (
+    <ServiceModalComponent
+      mdIndex={mdIndex}
+      onClick={onClick}
+      prevClick={prevClick}
+      nextClick={nextClick}
+      data={data[mdIndex - 1]}
+    ></ServiceModalComponent>
+  );
+  // switch (mdIndex) {
+  //   case 1:
+  //     return (
+  //       <ServiceModalComponent1 onClick={onClick}></ServiceModalComponent1>
+  //     );
+  //   case 2:
+  //     return (
+  //       <ServiceModalComponent2
+  //         onClick={onClick}
+  //         data={imsiData[1]}
+  //       ></ServiceModalComponent2>
+  //     );
+  //   case 3:
+  //     return (
+  //       <ServiceModalComponent3
+  //         onClick={onClick}
+  //         data={imsiData[2]}
+  //       ></ServiceModalComponent3>
+  //     );
+  //   case 4:
+  //     return (
+  //       <ServiceModalComponent4 onClick={onClick}></ServiceModalComponent4>
+  //     );
+  //   case 5:
+  //     return (
+  //       <ServiceModalComponent5 onClick={onClick}></ServiceModalComponent5>
+  //     );
+  //   case 6:
+  //     return (
+  //       <ServiceModalComponent6 onClick={onClick}></ServiceModalComponent6>
+  //     );
+  //   case 7:
+  //     return (
+  //       <ServiceModalComponent7 onClick={onClick}></ServiceModalComponent7>
+  //     );
+  //   case 8:
+  //     return (
+  //       <ServiceModalComponent8 onClick={onClick}></ServiceModalComponent8>
+  //     );
+  // }
 };
 
 export default ServiceModalContainer;
