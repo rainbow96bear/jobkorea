4d4327f250728fac886c8d0e115998880b8bf87b
Nyes98 <111405218+Nyes98@users.noreply.github.com>
2022-12-12 10:09:16 +0900

    header location move

--- jobkorea/src/components/companyMain/index.jsx
+++ jobkorea/src/components/companyMain/index.jsx
@@ -2,21 +2,42 @@ import { Routes, Route } from "react-router-dom";
 import CompanyMainContainer from "./companyMain/CompanyMainContainer";
 import RegistPostContainer from "./registPost/RegistPostContainer";
 import ManagePostContainer from "./managePost/ManagePostContainer";
+import styled from "styled-components";
+import HeaderContainer from "./companyMain/Header/Container";
+import FooterContainer from "./companyMain/Footer/Container";
+import { useState } from "react";
 
 export default function CompanyMain() {
+  const [loginIsClick, setLoginIsClick] = useState(false);
+
   return (
-    <div>
+    <Box>
+      <HeaderContainer
+        loginIsClick={loginIsClick}
+        setLoginIsClick={setLoginIsClick}
+      ></HeaderContainer>
       <Routes>
         <Route
           path="/"
-          element={<CompanyMainContainer></CompanyMainContainer>}></Route>
+          element={
+            <CompanyMainContainer
+              loginIsClick={loginIsClick}
+              setLoginIsClick={setLoginIsClick}
+            ></CompanyMainContainer>
+          }
+        ></Route>
         <Route
-          path="/registPost"
-          element={<RegistPostContainer></RegistPostContainer>}></Route>
+          path="/registPost/*"
+          element={<RegistPostContainer></RegistPostContainer>}
+        ></Route>
         <Route
           path="/managePost"
-          element={<ManagePostContainer></ManagePostContainer>}></Route>
+          element={<ManagePostContainer></ManagePostContainer>}
+        ></Route>
       </Routes>
-    </div>
+      <FooterContainer></FooterContainer>
+    </Box>
   );
 }
+
+const Box = styled.div``;
