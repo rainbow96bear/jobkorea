8b44ddab3f0ba820c93b209fa9e69ee27c55ba36
pippen11 <111038332+pippen11@users.noreply.github.com>
2022-12-14 10:40:28 +0900

    충돌 해결

--- jobkorea/src/modules/userInfo.js
+++ jobkorea/src/modules/userInfo.js
@@ -1,26 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-<<<<<<< HEAD
-const initialState = {
-  isLogged: false,
-  individual_name: null,
-  individual_id: null,
-};
-
-export const LoggedState = createSlice({
-  name: "user",
-  initialState,
-  reducers: {
-    loginAccount(state, action) {
-      state.isLogged = true;
-      state.individual_name = action.payload.individual_name;
-      state.individual_id = action.payload.individual_id;
-    },
-    logoutAccount(state) {
-      state.isLogged = false;
-      state.individual_name = null;
-      state.individual_id = null;
-=======
 let initialState = { value: 0 };
 
 const companyuserSlice = createSlice({
@@ -32,18 +11,10 @@ const companyuserSlice = createSlice({
     },
     logoutCompany(state) {
       state.value = 0;
->>>>>>> companyRegistFunc
     },
   },
 });
 
-<<<<<<< HEAD
-// Action creators are generated for each case reducer function
-export const { loginAccount, logoutAccount } = LoggedState.actions;
-
-export default LoggedState.reducer;
-=======
 export const companyreducer = companyuserSlice.reducer;
 
 export const action = companyuserSlice.actions;
->>>>>>> companyRegistFunc
